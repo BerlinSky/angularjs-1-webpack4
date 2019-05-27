c1ad37635d4d1334605c79a6824febc068b6390d
Berlin Sky <pedroyoung8989@gmail.com>
2019-05-27 09:25:43 -0700

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,24 @@
+const jsRules = {
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: [
+    {
+      loader: 'babel-loader'
+    }
+  ]
+}
+
 module.exports = {
   context: __dirname + '/app',
   entry: './index.js',
   output: {
     path: __dirname + '/app',
     filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [
+      jsRules,
+    ]
   }
 };
